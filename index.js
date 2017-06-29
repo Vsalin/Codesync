8aa7adc5deef586e02e8a76611bb0790b64c70ab
eaknoppanut <eaknoppanut@hotmail.com>
2017-06-29 14:08:25 +0700

    no message

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ var app = express()
 var http = require('http').Server(app);
 
 
+const testFolder = './';
+const fs = require('fs');
+
+
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html')
+
+  
 });
+
+app.post('/show',function(req,res){
+      fs.readdirSync(testFolder).forEach(file => {
+      console.log(file);
+    })
+      console.log('------------------------------------------------')
+      var  myfile =  fs.readdirSync(testFolder)
+      console.log(myfile);
+      res.send(myfile)
+
+    })
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
