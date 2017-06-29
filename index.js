d348d3ba2d70a054531dd6d145c16c4f11b3530f
ja-PC\ja <vesalin.thw@g.swu.ac.th>
2017-06-29 14:07:56 +0700

    no message

--- index.js
+++ index.js
@@ -14,19 +14,15 @@ app.get('/', function(req, res){
 });
 
 app.post('/show',function(req,res){
-    console.log('Yo')
-
-      fs.readdirSync(testFolder).forEach(file => {
-      console.log(file);
-    })
-      console.log('------------------------------------------------')
+    //   fs.readdirSync(testFolder).forEach(file => {
+    //   console.log(file);
+    // })
+    //   console.log('------------------------------------------------')
       var  myfile =  fs.readdirSync(testFolder)
       console.log(myfile);
       res.send(myfile)
 
     })
-
-
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
