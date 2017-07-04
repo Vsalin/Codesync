f0f0b570ac8ca2104765becd2dc7ce0bc83bc0d5
eaknoppanut <eaknoppanut@hotmail.com>
2017-07-04 10:30:03 +0700

    no message

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
 var express = require('express')
 var app = express()
 var http = require('http').Server(app);
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(bodyParser.json());
+var io = require('socket.io')(http);
 
 const testFolder = './';
 const fs = require('fs');
 
-if (!fs.exists)
-    fs.exists = path.exists;  
 
-app.get('/', function(req, res){
-   res.sendFile(__dirname + '/index.html')
-});
+  // var path = require("path")
+  // , url = require("url")
+  // , port = process.env.PORT || 8888
+  // , ip = process.env.IP || "0.0.0.0";
 
-app.post('/show',function(req,res){
+  app.use(bodyParser.json()); // for parsing application/json
+  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-      fs.readdirSync(testFolder).forEach(file => {
-      console.log(file);
-    })
+  app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+  });
+
+  app.post('/show',function(req,res){
       var  myfile =  fs.readdirSync(testFolder)
-      console.log(myfile);
       res.send(myfile)
-      myfile.push("..",".")
-      var html = myfile.map()
-
     })
+
 app.post('/showFile',function(req,res){
-  filename=req.body.b
- fs.readFile(filename,"utf8",function(err,data){
-    if (err) {
-      res.write(err + "\n");
-      res.end();
-      return;
-    } 
-     
-// console.log(data)  
-  res.json({
-    data: data
-  })
+   fileName = req.body.b
+   console.log('Sent from server  file: '+fileName)
+  // var context = fs.readFileSync(fileName, 'utf8')
+   
+   fs.readFile(fileName, 'binary', function (err,data) {
+      if (err) {
+      return console.log(err);
+    }
+        res.json({
+          data:data
+        })
+      });
 })
 
-    })
+io.on('connection', function(socket){
+    console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
