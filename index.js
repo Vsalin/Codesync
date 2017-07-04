dd3669790371c77eb4998a5bb3446430b77d7047
SuranartN <suranartnc@gmail.com>
2017-07-04 10:48:49 +0700

    add socket.io connection at client side

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3003, function(){
+  console.log('listening on *:3003');
 });
