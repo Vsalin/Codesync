51db5893429c2086ff9632c50aadf8e258b4d313
ja-PC\ja <vesalin.thw@g.swu.ac.th>
2017-07-06 16:24:18 +0700

    no message

--- index.js
+++ index.js
@@ -2,24 +2,12 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
-<<<<<<< HEAD
 
 var io = require('socket.io')(http);
 
-=======
-var io = require('socket.io')(http);
->>>>>>> e6fea53cae86e5d2268800e66f53fb6aafdd49d1
 const testFolder = './';
 const fs = require('fs');
- 
-  app.use(bodyParser.json()); // for parsing application/json
-  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-  app.get('/', function(req, res){  
-    res.sendFile(__dirname + '/index.html')
-  });
-
-<<<<<<< HEAD
   var path = require("path")
   , url = require("url")
   , port = process.env.PORT || 8888
@@ -37,7 +25,6 @@ const fs = require('fs');
       res.send(myfile)
     })
 
-
 app.post('/showfile',function(req,res){
    fileName = req.body.a
    console.log('Sent from server  file: '+fileName)
@@ -52,8 +39,6 @@ app.post('/showfile',function(req,res){
         })
       });
 })
-
-
 
 io.on('connection', function(socket){
   //var socks = [];
@@ -78,55 +63,9 @@ io.on('connection', function(socket){
   //   console.log('new body');
   //   body = body_;
   // });
-
 });
-
-
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-=======
-  app.post('/show',function(req,res){
-      var  myfile =  fs.readdirSync(testFolder)
-      res.send(myfile)
-    })
 
-app.post('/showFile',function(req,res){
-   fileName = req.body.b
-   console.log('Sent from server  file: '+fileName)
- 
-   fs.readFile(fileName, 'binary', function (err,data) {
-      if (err) {
-      return console.log(err);
-    }
-        res.json({
-          data:data
-        })
-      });
-})
-var body="";
-  io.sockets.on('connection', function(socket){
-    console.log('a user connected');
-    
-    socket.emit('refrest',{body:body})
-
-    socket.on('refrest',function(body_){
-      body=body_
-    })
-
-    socket.on('disconnect', function(){
-    console.log('user disconnected');
-    });
->>>>>>> e6fea53cae86e5d2268800e66f53fb6aafdd49d1
-
-  socket.on('change', function (op) {
-      console.log(op);
-      if(op.action == 'insert' || op.action == 'remove'){
-         socket.broadcast.emit('change', op);
-      }
-    })
-  })
-http.listen(3003, function(){
-  console.log('listening on *:3003');
-});
