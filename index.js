e456a92b9b4434b1e076f9619e1c52f302a11462
ja-PC\ja <vesalin.thw@g.swu.ac.th>
2017-07-06 16:01:53 +0700

    edit code

--- index.js
+++ index.js
@@ -1,28 +1,76 @@
 var express = require('express')
 var app = express()
 var http = require('http').Server(app);
+var bodyParser = require('body-parser');
 
+var io = require('socket.io')(http);
 
 const testFolder = './';
 const fs = require('fs');
 
 
-app.get('/', function(req, res){
-   res.sendFile(__dirname + '/index.html')
+  var path = require("path")
+  , url = require("url")
+  , port = process.env.PORT || 8888
+  , ip = process.env.IP || "0.0.0.0";
 
-  
+  app.use(bodyParser.json()); // for parsing application/json
+  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+  app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+  });
+
+  app.post('/list',function(req,res){
+      var  myfile =  fs.readdirSync(testFolder)
+      res.send(myfile)
+    })
+
+
+app.post('/showfile',function(req,res){
+   fileName = req.body.a
+   console.log('Sent from server  file: '+fileName)
+  // var context = fs.readFileSync(fileName, 'utf8')
+   
+   fs.readFile(fileName, 'binary', function (err,data) {
+      if (err) {
+      return console.log(err);
+    }
+        res.json({
+          data:data
+        })
+      });
+})
+
+
+
+io.on('connection', function(socket){
+  //var socks = [];
+  //var body = "";
+  console.log('a user connected');
+  //socks.push(socket);
+  // socket.emit('refresh', {body: body});
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('change', function (op) {
+    console.log(op);
+    if (op.action == 'insert' || op.action == 'remove' ) {
+        //console.log('I found insert || remove')
+        socket.emit('change', op);
+       // socket.broadcast.emit('change', op);
+     };
+  });
+ 
+  // socket.on('refresh', function (body_) {
+  //   console.log('new body');
+  //   body = body_;
+  // });
+
 });
 
-app.post('/show',function(req,res){
-    //   fs.readdirSync(testFolder).forEach(file => {
-    //   console.log(file);
-    // })
-    //   console.log('------------------------------------------------')
-      var  myfile =  fs.readdirSync(testFolder)
-      console.log(myfile);
-      res.send(myfile)
 
-    })
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
