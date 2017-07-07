4d439980b6199a11d805c9f5b44b67f538eafb95
eaknoppanut <eaknoppanut@hotmail.com>
2017-07-07 08:56:02 +0700

    no message

--- index.js
+++ index.js
@@ -2,26 +2,34 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
+
 var io = require('socket.io')(http);
+
 const testFolder = './';
 const fs = require('fs');
- 
+
+  var path = require("path")
+  , url = require("url")
+  , port = process.env.PORT || 8888
+  , ip = process.env.IP || "0.0.0.0";
+
   app.use(bodyParser.json()); // for parsing application/json
   app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-  app.get('/', function(req, res){  
+  app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html')
   });
 
-  app.post('/show',function(req,res){
+  app.post('/list',function(req,res){
       var  myfile =  fs.readdirSync(testFolder)
       res.send(myfile)
     })
 
-app.post('/showFile',function(req,res){
-   fileName = req.body.b
+app.post('/showfile',function(req,res){
+   fileName = req.body.a
    console.log('Sent from server  file: '+fileName)
- 
+  // var context = fs.readFileSync(fileName, 'utf8')
+   
    fs.readFile(fileName, 'binary', function (err,data) {
       if (err) {
       return console.log(err);
@@ -31,20 +39,33 @@ app.post('/showFile',function(req,res){
         })
       });
 })
-var body="";
-  io.sockets.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
+
+io.on('connection', function(socket){
+  //var socks = [];
+  //var body = "";
+  console.log('a user connected');
+  //socks.push(socket);
+  // socket.emit('refresh', {body: body});
+  socket.on('disconnect', function(){
     console.log('user disconnected');
-    });
+  });
 
   socket.on('change', function (op) {
-      console.log(op);
-      if(op.action == 'insert' || op.action == 'remove'){
-         socket.broadcast.emit('change', op);
-      }
-    })
-  })
-http.listen(3003, function(){
-  console.log('listening on *:3003');
+    console.log(op);
+    if (op.action == 'insert' || op.action == 'remove' ) {
+        //console.log('I found insert || remove')
+        socket.emit('change', op);
+       // socket.broadcast.emit('change', op);
+     };
+  });
+ 
+  // socket.on('refresh', function (body_) {
+  //   console.log('new body');
+  //   body = body_;
+  // });
 });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
