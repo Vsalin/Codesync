bd993743a412f8a1da162e18ab731e4675a532e5
ja-PC\ja <vesalin.thw@g.swu.ac.th>
2017-07-07 15:32:39 +0700

    no message

--- index.js
+++ index.js
@@ -20,16 +20,10 @@ const fs = require('fs');
     res.sendFile(__dirname + '/index.html')
   });
 
-//   app.post('/list',function(req,res){
-//       var  myfile =  fs.readdirSync(testFolder)
-//       res.send(myfile)
-//     })
-
 // app.post('/showfile',function(req,res){
 //    //path
 //    fileName = req.body.a
 //    console.log('Sent from server  file: '+fileName)
-//   // var context = fs.readFileSync(fileName, 'utf8')
    
 //    fs.readFile(fileName, 'binary', function (err,data) {
 //       if (err) {
@@ -39,18 +33,22 @@ const fs = require('fs');
 //           data:data
 //         })
 //       });
-// })
+//})
 
 io.on('connection', function(socket){
   console.log('a user connected');
     var  myfile =  fs.readdirSync(testFolder)
     io.emit('listdir',myfile)
 
+    var datafile = fs.readFileSync('README.md','binary');
+    console.log(datafile);
+    io.emit('readfile',datafile)
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-
 });
+
 
  //chokidar start
 //   var watcher = chokidar.watch('README.md', {
