76e5fa6afdd282e04a9c46c1706d58e0b2c6f972
ja-PC\ja <vesalin.thw@g.swu.ac.th>
2017-07-07 14:55:08 +0700

    *-* use socket to show dir

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
-<<<<<<< HEAD
-=======
-
-var chokidar = require('chokidar');
-
->>>>>>> 4615e2ebc05e603fa38fb1e4edc8189c7e38f6f8
 var io = require('socket.io')(http);
 var chokidar = require('chokidar');
 
@@ -26,57 +20,48 @@ const fs = require('fs');
     res.sendFile(__dirname + '/index.html')
   });
 
-  app.post('/list',function(req,res){
-      var  myfile =  fs.readdirSync(testFolder)
-      res.send(myfile)
-    })
+//   app.post('/list',function(req,res){
+//       var  myfile =  fs.readdirSync(testFolder)
+//       res.send(myfile)
+//     })
 
-app.post('/showfile',function(req,res){
-   //path
-   fileName = req.body.a
-   console.log('Sent from server  file: '+fileName)
-  // var context = fs.readFileSync(fileName, 'utf8')
+// app.post('/showfile',function(req,res){
+//    //path
+//    fileName = req.body.a
+//    console.log('Sent from server  file: '+fileName)
+//   // var context = fs.readFileSync(fileName, 'utf8')
    
-   fs.readFile(fileName, 'binary', function (err,data) {
-      if (err) {
-      return console.log(err);
-    }
-        res.json({
-          data:data
-        })
-      });
-})
+//    fs.readFile(fileName, 'binary', function (err,data) {
+//       if (err) {
+//       return console.log(err);
+//     }
+//         res.json({
+//           data:data
+//         })
+//       });
+// })
 
 io.on('connection', function(socket){
   console.log('a user connected');
+    var  myfile =  fs.readdirSync(testFolder)
+    io.emit('listdir',myfile)
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 
-<<<<<<< HEAD
-=======
-  socket.on('change', function (op) {
-    console.log(op);
-    if (op.action == 'insert' || op.action == 'remove' ) {
-        //console.log('I found insert || remove')
-        socket.emit('change', op);
-       // socket.broadcast.emit('change', op);
-     };
-  });
->>>>>>> 4615e2ebc05e603fa38fb1e4edc8189c7e38f6f8
 });
 
  //chokidar start
-  var watcher = chokidar.watch('README.md', {
-    ignored: /(^|[\/\\])\../,
-    persistent: true
-  });
-  watcher
-  .on('add', path => console.log(`File ${path} has been added`))
-  .on('change', path => console.log(`File ${path} has been changed`));
+//   var watcher = chokidar.watch('README.md', {
+//     ignored: /(^|[\/\\])\../,
+//     persistent: true
+//   });
+//   watcher
+//   .on('add', path => console.log(`File ${path} has been added`))
+//   .on('change', path => console.log(`File ${path} has been changed`));
 
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
