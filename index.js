53a74787cc037b4de85376373c29456635f1bab5
ja-PC\ja <vesalin.thw@g.swu.ac.th>
2017-07-11 13:35:39 +0700

    no message

--- index.js
+++ index.js
@@ -24,40 +24,65 @@ const fs = require('fs');
 //    //path
 //    fileName = req.body.a
 //    console.log('Sent from server  file: '+fileName)
-   
-//    fs.readFile(fileName, 'binary', function (err,data) {
-//       if (err) {
-//       return console.log(err);
-//     }
-//         res.json({
-//           data:data
-//         })
-//       });
+//    room = fileName
+//   //  fs.readFile(fileName, 'binary', function (err,data) {
+//   //     if (err) {
+//   //     return console.log(err);
+//   //   }
+//   //       res.json({
+//   //         data:data
+//   //       })
+//   //     });
 
-// })
+//   io.on('connection', function(socket){
+//     socket.join(room);
+//   });
+
+//     var watcher = chokidar.watch(fileName, {
+//         ignored: /(^|[\/\\])\../,
+//         persistent: true
+//         });
+//     watcher
+//       .on('add', path => console.log(`File ${path} has been added`))
+//       .on('change', path => {
+//     var datafile = fs.readFileSync(fileName,'binary');
+//     console.log(datafile);
+//     io.to(room).emit('readfile',datafile)
+//   });
+
+//})
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  
     var  myfile =  fs.readdirSync(testFolder)
     io.emit('listdir',myfile)
 
-    var datafile = fs.readFileSync('README.md','binary');
-    console.log(datafile);
-    io.emit('readfile',datafile)
 
+
+
+    var datafile = fs.readFileSync('README.md','binary');
+    io.emit('readfile',datafile)
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+
+  socket.on('Callpath',function(pathFile){
+    console.log(pathFile);
+
+    
+
+  })
+
+
+
 });
 
-//  chokidar start
+ //chokidar start
   var watcher = chokidar.watch('README.md', {
     ignored: /(^|[\/\\])\../,
     persistent: true
   });
   watcher
-  .on('add', path => console.log(`File ${path} has been added`))
   .on('change', path => {
     var datafile = fs.readFileSync('README.md','binary');
     console.log(datafile);
